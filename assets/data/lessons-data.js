6b96bba908cdca50b1490d6c6dc5b719536af052
Pam Dingal <pamelakayedingal@Pammy.local>
2026-03-12 21:52:27 +0800

    added complete lessons in json

--- assets/data/lessons-data.js
+++ assets/data/lessons-data.js
@@ -335,55 +335,320 @@ window.GODUCATE_LESSONS = {
     }
     
   ],//PRIMARY 2
-  primary2: [
+  primary2: [//Communication Skills
     {
-      title: "Primary 2 English",
-      description: "Sentence building and reading comprehension.",
-      link: "https://drive.google.com/file/d/EXAMPLE_PRIMARY2_1/view"
+      title: "1 Matching Alphabets",
+      description: "Pair the letters and make magic words!",
+      link: "https://drive.google.com/open?id=1qwDOSWbl-KrNWPUuoF4KhRdFVlY1vBcn&usp=drive_copy"
     },
     {
-      title: "Primary 2 Mathematics",
-      description: "Word problems and place value concepts.",
-      link: "https://drive.google.com/file/d/EXAMPLE_PRIMARY2_2/view"
+      title: "2 What is the Story",
+      description: "Solve the puzzle, find the story!",
+      link: "https://drive.google.com/open?id=1HRoCUiILg62MM8U48yigkS0dhGOKvev9&usp=drive_copy"
     },
     {
-      title: "Primary 2 Science",
-      description: "Simple experiments and observations.",
-      link: "https://drive.google.com/file/d/EXAMPLE_PRIMARY2_3/view"
+      title: "3 Sshhh Consonant Digraphs",
+      description: "Listen close... the letters whisper!",
+      link: "https://drive.google.com/open?id=12TE3Kzzd5JLgC4a8HKQAKg_SXDMElAUe&usp=drive_copy"
+    },
+    {
+      title: "4 Poetry in Rhymes",
+      description: "Rhyme time! Fun words in lines.",
+      link: "https://drive.google.com/open?id=1cKkekG8nhfU26ztQ6uR1oFMDNs2yf266&usp=drive_copy"
+    },
+    {
+      title: "5 This and That and These and Those",
+      description: "Point, choose and learn them all!",
+      link: "https://drive.google.com/open?id=1Olypwxxi0e2eVHFWe1bX-_Svbt-19-bI&usp=drive_copy"
+    },
+    {
+      title: "6 Long Vowel Sounds and Nouns",
+      description: "Stretch those vowels and name everything!",
+      link: "https://drive.google.com/open?id=1eD4uyB9FUuhAJocz_Jl_zp7h1KNEcmZT&usp=drive_copy"
+    },
+    {
+      title: "7 Gem to Germ",
+      description: "Tiny wonders of the world, explored!",
+      link: "https://drive.google.com/open?id=1iA7AIwc5DtC__OBRIFUQhhntunZWH5js&usp=drive_copy"
+    },
+    {
+      title: "8 I am a Boy with a Toy",
+      description: "Read, play and tell your story!",
+      link: "https://drive.google.com/open?id=1ob5C4zHwcEsvqk0PkD03Bx7qGlmMS9Q2&usp=drive_copy"
+    },
+    {
+      title: "9 My Pet is Parrot",
+      description: "Meet the colorful talker in your story!",
+      link: "https://drive.google.com/open?id=1zWYj75cG3r-88L7UXXEHiJAmtH7YSQuV&usp=drive_copy"
+    },
+    {
+      title: "10 Is and Are",
+      description: "Little helpers that make sentences shine!",
+      link: "https://drive.google.com/open?id=1H24fPRVtC-SWFs9FxDs44sz74MvWhUZY&usp=drive_copy"
+    },//Scientific and Critical Thinking Skills
+    {
+      title: "1 Things Around Us",
+      description: "Eplore the world near you!",
+      link: "https://drive.google.com/open?id=1_APSR_gX2uJ_PXXzkEoPEulkqKo-Py4J&usp=drive_copy"
+    },
+    {
+      title: "2 Do You Hear What I Hear?",
+      description: "Listen closely and find the sounds!",
+      link: "https://drive.google.com/open?id=1htsqMrSU2lGBReauhhJuoMUeOyBbUnQl&usp=drive_copy"
+    },
+    {
+      title: "3 Hmmm Yummy Foods",
+      description: "Taste, see, and learn about yummy treats!",
+      link: "https://drive.google.com/open?id=1Z2zno9yAYJBmbwfS53mLT5mYwU3whgFn&usp=drive_copy"
+    },
+    {
+      title: "4 Animals Around Us",
+      description: "Meet furry, featheres, and scaly friends!",
+      link: "https://drive.google.com/open?id=1wBmNol4evK-8bI_nCy-jA9l4uggafaQN&usp=drive_copy"
+    },
+    {
+      title: "5 Plants Around Us",
+      description: "Discover green wonders all around!",
+      link: "https://drive.google.com/open?id=13Ue0netge5vLD0CjwGfddgvO_c1VTFzw&usp=drive_copy"
+    },
+    {
+      title: "6 My Health I Take Care",
+      description: "Fun ways to stay strong and healthy!",
+      link: "https://drive.google.com/open?id=1ZV0Ss2wPO48Dud68RxnHc8LJ27-UBf7A&usp=drive_copy"
+    },
+    {
+      title: "7 Our Beautiful World",
+      description: "See the magic in the nature everywhere.",
+      link: "https://drive.google.com/open?id=1nDVt_vES3mPRZRyEyZS0epjED_LIvXqw&usp=drive_copy"
+    },
+    {
+      title: "8 Flu and You",
+      description: "Learn to fight germs like a hero!",
+      link: "https://drive.google.com/open?id=1BBJ-fW9uPyExRfpCdQ2ertepBw7l6unj&usp=drive_copy"
+    },
+    {
+      title: "9 What is Energy",
+      description: "Discover what makes things go zoom!",
+      link: "https://drive.google.com/open?id=19Ys0BFEbbN77QyPSwAJncHrE_1AfjK7i&usp=drive_copy"
+    },
+    {
+      title: "10 Machines Around Us",
+      description: "Explore helpful machines in action.",
+      link: "https://drive.google.com/open?id=1cQmdxRdzXYgFBWeP9Tjl77V5-QH5aFi2&usp=drive_copy"
+    },//Mathematical and Problem Solving Skills
+    {
+      title: "1 I've Got 100",
+      description: "Count, match, and celebrate!",
+      link: "https://drive.google.com/open?id=1yv9E61I2WLEpLlD-yQxNKAbe7Eznfort&usp=drive_copy"
+    },
+    {
+      title: "2 Where Do the Numbers Belong",
+      description: "Place numbers in the perfect spot",
+      link: "https://drive.google.com/open?id=1vk6EmiMjCMvJ4cRXk5DojNmqA4ZbzN75&usp=drive_copy"
+    },
+    {
+      title: "3 Odd and Even Numbers",
+      description: "Odd of even? Let's sort them out!",
+      link: "https://drive.google.com/open?id=14xJvk69EnZrVZYX0C0mvfXewn0olkSSL&usp=drive_copy"
+    },
+    {
+      title: "4 A Thousand's Place",
+      description: "Big numbers, big fun!",
+      link: "https://drive.google.com/open?id=1bEiJi5u1Eo0fCkh0nlr2N-QE_8HyUnXB&usp=drive_copy"
+    },
+    {
+      title: "5 Carrying and Borrowing",
+      description: "Add and subreact like a math wizard!",
+      link: "https://drive.google.com/open?id=1oCynF5JFidCrgc7VPtFVhsPm7FfrPs5W&usp=drive_copy"
+    },
+    {
+      title: "6 Multiplication",
+      description: "Multiply to make magic happen!",
+      link: "https://drive.google.com/open?id=1IvIIDu6wLVNa_4qaoV9SVXiK1Ocnm2l5&usp=drive_copy"
+    },
+    {
+      title: "7 Division",
+      description: "Share and split to solve the mystery.",
+      link: "https://drive.google.com/open?id=109uS2wussOyGPh9k93lTREmVWd2NCNvo&usp=drive_copy"
+    },
+    {
+      title: "8 Fraction and Solid Shapes",
+      description: "Cut, shape, and explore fun fractions!",
+      link: "https://drive.google.com/open?id=1bmhwCt7-6i_ihmYtaCXBIiBqkXcHooZG&usp=drive_copy"
+    },
+    {
+      title: "9 Measurement and Graph",
+      description: "Measure, draw, and see the story!",
+      link: "https://drive.google.com/open?id=1NchbG1FWAtYyGCsfT33YjbDO7myqeYJH&usp=drive_copy"
+    },
+    {
+      title: "10 Using Time and Money Wisely",
+      description: "Clock it and spend smartly!",
+      link: "https://drive.google.com/open?id=1ci-FbRMV4726ykDJ9fKNIWfUH_4jV7VQ&usp=drive_copy"
+    },//Understanding Self and Society
+    {
+      title: "1 My Looks, I'm Unique",
+      description: "Be you, shine bright!",
+      link: "https://drive.google.com/open?id=1xWAIiBk9vGJtn9ORVgNDUReSartmQ_jk&usp=drive_copy"
+    },
+    {
+      title: "2 My Growth",
+      description: "Watch how I grow everyday!",
+      link: "https://drive.google.com/open?id=16zkH9zS13f9UdfiJs6s10eK1YwBTqYQE&usp=drive_copy"
+    },
+    {
+      title: "My Expression",
+      description: "Show your feelinges, loud and proud!",
+      link: "https://drive.google.com/open?id=12AmHXlRlTwOnXGtcsfR-hlJKbhsaxZ9S&usp=drive_copy"
+    },
+    {
+      title: "4 People Helping Us",
+      description: "Helpers everywhere make life better!",
+      link: "https://drive.google.com/open?id=1uNXS8eIwpJSgWnUNNrpPOwmQdbkbRXw1&usp=drive_copy"
+    },
+    {
+      title: "5 Roles of People Helping Us",
+      description: "Discover what everyone can do!",
+      link: "https://drive.google.com/open?id=1cH1sSsJu3qH3Rn_e9x4LqMnC7br31F4k&usp=drive_copy"
+    },
+    {
+      title: "6 Where Am I Going",
+      description: "Dream Big and plan your path!",
+      link: "https://drive.google.com/open?id=1ghPlL-f-jcsC3zkotJnpkXgOg4rnMexZ&usp=drive_copy"
+    },
+    {
+      title: "7 Dada Manana",
+      description: "Later",
+      link: "https://drive.google.com/open?id=1t05rivM4C8DtCgY3ze79ShEccU-VspFX&usp=drive_copy"
+    },
+    {
+      title: "8 I am a Filipino",
+      description: "Celebrat being proud and unique",
+      link: "https://drive.google.com/open?id=1poAueE0hzmM4qCwd491tq1tSnszkKDMb&usp=drive_copy"
+    },
+    {
+      title: "9 Flag, Anthem, and Pledges",
+      description: "Honor our nation with pride!",
+      link: "https://drive.google.com/open?id=1xr-R9mvusFmDO_uUmMFx61zK-nglizKI&usp=drive_copy"
+    },
+    {
+      title: "10 True Heroes",
+      description: "Every act of courage counts!",
+      link: "https://drive.google.com/open?id=1IXjk3TGdxs4eXzBjMrBXJdMeU-olEh3q&usp=drive_copy"
     }
   ],
-  primary3: [
+  primary3: [//Communication Skills
     {
-      title: "Primary 3 English",
-      description: "Grammar practice and guided writing.",
-      link: "https://drive.google.com/file/d/EXAMPLE_PRIMARY3_1/view"
+      title: "1 Phonological Awareness",
+      description: "Hear the sounds, play with words!",
+      link: "https://drive.google.com/drive/folders/1QaEz6Rvkro9JMroq0vTjz7jiDa1sjhkT?usp=drive_link"
     },
     {
-      title: "Primary 3 Mathematics",
-      description: "Multiplication and division review.",
-      link: "https://drive.google.com/file/d/EXAMPLE_PRIMARY3_2/view"
+      title: "2 Phonics and Word Study",
+      description: "Mix, match, and master letters!",
+      link: "https://drive.google.com/drive/folders/1ukoxJmu03qLAj6wNTG0jzOKgdFLj3ani?usp=drive_link"
     },
     {
-      title: "Primary 3 Social Studies",
-      description: "Community and culture learning activities.",
-      link: "https://drive.google.com/file/d/EXAMPLE_PRIMARY3_3/view"
+      title: "3 Vocabulary and Word Knowledge",
+      description: "Learn new words, make magic!",
+      link: "https://drive.google.com/drive/folders/1tg0dzFwF-v3VMavqJ-tiR8lLNxyo5t0r?usp=drive_link"
+    },
+    {
+      title: "4 Book and Print Knowledge",
+      description: "Books are fun- let's explore!",
+      link: "https://drive.google.com/drive/folders/1OaZ4Z-fYdhPLyUHLT1cIsh1pqbDkdRSf?usp=drive_link"
+    },//Scientific and Critical Thinking Skills
+    {
+      title: "1 Matters in Daily Life",
+      description: "Explore the stuff all around you!",
+      link: "https://drive.google.com/drive/folders/1Sz9XA58heqLbRSK2aPEUotq7pMUILjmM?usp=drive_link"
+    },
+    {
+      title: "2 Three States of Matter",
+      description: "Solid, liquid, gas- let's investigate!",
+      link: "https://drive.google.com/drive/folders/1nRxCinFYyAThpiC0kw0yLfGvDYgxEXhX?usp=drive_link"
+    },
+    {
+      title: "3 Scientific Investigation",
+      description: "Be a mini scientist, discover wonders!",
+      link: "https://drive.google.com/drive/folders/1vfChUIJtd98TW8qFZzcu_0Eed2-Rv6dn?usp=drive_link"
+    },
+    {
+      title: "4 Body Systems in Animals",
+      description: "See how animals tick inside!",
+      link: "https://drive.google.com/drive/folders/1VeWHFnhmVyBi7QpNQYVWlhgtANyN75Bm?usp=drive_link"
+    },
+    {
+      title: "5 Plants, Animals, and Microorganisms",
+      description: "Tiny and bid creatures, all explored!",
+      link: "https://drive.google.com/drive/folders/1n1_NcZm51BqwnDg8tJbhVwR1x0pC4yCE?usp=drive_link"
+    },
+    {
+      title: "6 Life Cycle of Living Things",
+      description: "From tiny beginnings to full-grown wonders!",
+      link: "https://drive.google.com/drive/folders/15-40R7F5V3rvf6nFKNP7C3es-bDJ3HKd?usp=drive_link"
+    },
+    {
+      title: "7 Specialized Strcutures in Plants",
+      description: "Parts of plants that do amazing jobs!",
+      link: "https://drive.google.com/drive/folders/1cV-MI_zTPVBvE_49PjBGDTHqQ5kp7elR?usp=drive_link"
+    },//Mathematical and Problem Solving Skills
+    {
+      title: "1 2 - Dimensional Shapes and Their Features",
+      description: "Squares, triangles, make shapes shine!",
+      link: "https://drive.google.com/drive/folders/1f-_rWDxtR6Gugh9xKlu4HGgP3g11_8es?usp=drive_link"
+    },
+    {
+      title: "2 Area of Squares and Rectangles",
+      description: "Measure the spaces, make shapes shine!",
+      link: "https://drive.google.com/drive/folders/1DT8gLHcXqZyKBLRvfoWwgwXkst-2RnRB?usp=drive_link"
+    },
+    {
+      title: "3 Specialized Strcutures in Plants",
+      description: "Shapes in nature? Let's explore!",
+      link: "https://drive.google.com/drive/folders/19cM6ZwsKDAUpHhY73oxAjZve0rYEUdjw?usp=drive_link"
+    },
+    {
+      title: "4 Points, Lines, Line SEegments and Rays",
+      description: "Connect the dots and draw magic!",
+      link: "https://drive.google.com/drive/folders/1Oogk-XtOtK4KjqcJsIqvddLodXIRGDnw?usp=drive_link"
+    },
+    {
+      title: "5 Whole Numbers Up to 10,000",
+      description: "Big numbers, big adventures!",
+      link: "https://drive.google.com/drive/folders/1bc1uJ_gVrk5GR9v3vkHTmWdoJRX6YJA9?usp=drive_link"
     }
   ],
-  advanced: [
+  lifeskills: [
     {
-      title: "Advanced English",
-      description: "Reading analysis and extended writing tasks.",
-      link: "https://drive.google.com/file/d/EXAMPLE_ADVANCED_1/view"
+      title: "1 Basic Housekeeping",
+      description: "Sweep, dust, and make your space shine!",
+      link: "https://drive.google.com/drive/folders/1QWEP4ZXnt_c1Lk_ur87855X4TGzelB1B?usp=sharing"
     },
     {
-      title: "Advanced Mathematics",
-      description: "Fractions, decimals, and problem-solving.",
-      link: "https://drive.google.com/file/d/EXAMPLE_ADVANCED_2/view"
+      title: "2 Basic Cookery",
+      description: "Mix, stir, and create yummy treats!",
+      link: "https://drive.google.com/drive/folders/1PbO2zBLm_SI8qXEbtXJVe-1L8umkG1hV?usp=sharing"
     },
     {
-      title: "Advanced Integrated Skills",
-      description: "Project-based lessons across subjects.",
-      link: "https://drive.google.com/file/d/EXAMPLE_ADVANCED_3/view"
+      title: "3 Basic Gardening",
+      description: "Plant, water, and watch magic grow!",
+      link: "https://drive.google.com/drive/folders/1paD4B4CyrrqYIEHmBK-90jWpFXJ9a5PO?usp=drive_link"
+    },
+    {
+      title: "4 Basic Airconditioning",
+      description: "Cool down and learn to keep things fresh!",
+      link: "https://drive.google.com/drive/folders/1gd9doIUwP0ds5ioP37E3QTI6ghZanbcV?usp=drive_link"
+    },
+    {
+      title: "5 Basic Automotives",
+      description: "Wheels, engines, and little car adventures!",
+      link: "https://drive.google.com/drive/folders/1im5b5rkt2SqshlxsEUQMRU3T1p2ZrcQB?usp=drive_link"
+    },
+    {
+      title: "6 Basic Computer Skills",
+      description: "Click, type, and explore the digital world!",
+      link: "https://drive.google.com/drive/folders/1Aj9PHzHGbfMOAMyccBaRaE2mjW0JKM_i?usp=drive_link"
     }
   ]
 };
